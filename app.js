ae1f962af7891dddbf94fd5daedcd940b65bfb1c
gobltpqls99 <gobltpqls99@naver.com>
2021-02-19 18:20:08 +0900

    layout

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-requestAnimationFrame('dotenv').config();
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.listen(process.env.PORT, () => {
 	console.log('=====================');
@@ -12,3 +13,8 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.locals.pretty = true;
 
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.get('/home', (res, req) => {
+	req.render('home/main')
+})
