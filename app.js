3a99a446ab8d3994d1f083e264627e3b02f75b93
gobltpqls99 <gobltpqls99@naver.com>
2021-03-01 22:39:19 +0900

    ..

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+const { default: Typed } = require("typed.js");
 
 app.listen(process.env.PORT, () => {
 	console.log('=====================');
--- public/js/home.js
+++ public/js/home.js
@@ -1,9 +1,34 @@
+// let target = document.querySelector("#typing");
+// let srtingArr = ["웹 개발자", "프론트엔드 개발자", "웹 퍼블리셔"];
+// let selectString = stringArr[Math.floor(Math.random() * stringArr.length)];
+// let selectStringArr = selectString.split("");
 
-var str = "프론트엔드 개발자";
-var c = 0;
 
-setInterval(function() {
-    if(c<10)
-        $('#typing').append(str[c]);
-        c++;
-}, 300);
+
+// function typing(randomArr) {
+//     if(randomArr.length > 0){
+//         target.textContent += randomArr.shift();
+//         setTimeout(function(){
+//             typing(randomArr);
+//         }, 300); 
+//     }
+// }
+// typing(selectStringArr);
+
+// function blink() {
+//     target.classList.toggle("active");
+// }
+// setInterval(blink, 500);
+
+
+// var options = {
+//     strings: ["프론트엔드 개발자", "웹 개발자", "웹 퍼블리셔"],
+//     typeSpeed: 40,
+//     backSpeed: 100,
+//     fadeOut: false,
+//     smartBackspace: true,
+//     cursorChar: '_',
+//     loop: true
+// };
+// $("#typing").append(options);
+// var typed = new Typed('#typing', options);
