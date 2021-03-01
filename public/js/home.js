eca794ec8ca5a660adc8ee2105e1606057a2fd29
gobltpqls99 <gobltpqls99@naver.com>
2021-03-01 22:42:46 +0900

    ..

--- public/js/home.js
+++ public/js/home.js
@@ -1,24 +1,22 @@
-// let target = document.querySelector("#typing");
-// let srtingArr = ["웹 개발자", "프론트엔드 개발자", "웹 퍼블리셔"];
-// let selectString = stringArr[Math.floor(Math.random() * stringArr.length)];
-// let selectStringArr = selectString.split("");
+let target = document.querySelector("#typing");
+let stringArr = ["웹 개발자", "프론트엔드 개발자", "웹 퍼블리셔"];
+let selectString = stringArr[Math.floor(Math.random() * stringArr.length)];
+let selectStringArr = selectString.split("");
 
+function typing(randomArr) {
+    if(randomArr.length > 0){
+        target.textContent += randomArr.shift();
+        setTimeout(function(){
+            typing(randomArr);
+        }, 300); 
+    }
+}
+typing(selectStringArr);
 
-
-// function typing(randomArr) {
-//     if(randomArr.length > 0){
-//         target.textContent += randomArr.shift();
-//         setTimeout(function(){
-//             typing(randomArr);
-//         }, 300); 
-//     }
-// }
-// typing(selectStringArr);
-
-// function blink() {
-//     target.classList.toggle("active");
-// }
-// setInterval(blink, 500);
+function blink() {
+    target.classList.toggle("active");
+}
+setInterval(blink, 500);
 
 
 // var options = {
@@ -30,5 +28,4 @@
 //     cursorChar: '_',
 //     loop: true
 // };
-// $("#typing").append(options);
 // var typed = new Typed('#typing', options);
